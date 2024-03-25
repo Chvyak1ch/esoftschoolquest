215635c1f5db02222900dc2cc10ee12da2a0c58c
Chvyak1ch <164943047+Chvyak1ch@users.noreply.github.com>
2024-03-26 02:35:47 +0500

    Update zadanie1.js

--- zadanie1.js
+++ zadanie1.js
@@ -1,1 +1,58 @@
-html
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        .house {
+            position: relative;
+            width: 200px;
+            height: 200px;
+            background: #f88e03;
+            margin: 0 auto;
+            margin-top: 150px;
+        }
+
+        .roof {
+            position: relative; z-index: 3;
+            width: 0;
+            height: 0;
+            border-left: 100px solid transparent;
+            border-right: 100px solid transparent;
+            border-bottom: 100px solid #A52A2A;
+            top: -100px;
+            left: 0;
+            right: 0;
+            margin: 0 auto;
+        }
+
+        .window {
+            position: relative;
+            width: 70px;
+            height: 70px;
+            background: #87CEEB;
+            border-radius: 50%;
+            top: -30px;
+            left: 0;
+            right: 0;
+            margin: auto;
+        }
+
+        .chimney {
+            position: absolute; z-index: 2;
+            width: 30px;
+            height: 70px;
+            background: #6d6c6c;
+            top: -80px;
+            right: 20px;
+        }
+    </style>
+</head>
+<body>
+
+<div class="house">
+    <div class="roof"></div>
+    <div class="window"></div>
+    <div class="chimney"></div>
+</div>
+
+</body>
+</html>
